6a8da32ef6e6bd00ffe7b647bf926b09a25f41d9
Yusuf Felly <beebarss.saj.ma@gmail.com>
2022-07-23 16:57:38 +0100

    Modified test description

--- server/todo-node/src/__tests__/taskController.test.ts
+++ server/todo-node/src/__tests__/taskController.test.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import TaskController from "../controllers/taskController";
 
-describe("Task routes", () => {
-  it("responds to a GET request", async () => {
+describe("Task controller", () => {
+  it("handles errors correctly (GET)", async () => {
     const mockNext: NextFunction = jest.fn();
     const taskController = new TaskController();
     const req = {} as Request;
@@ -11,7 +11,7 @@ describe("Task routes", () => {
     await taskController.getTasks(req, res, mockNext);
     expect(mockNext).toHaveBeenCalledTimes(1);
   });
-  it("responds to a fetch request", async () => {
+  it("handles errors correctly (POST)", async () => {
     const mockNext: NextFunction = jest.fn();
     const taskController = new TaskController();
     const req = {
