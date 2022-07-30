f15f5b56a07b0742c2d66f2e60acda96fe43940e
Yusuf Felly <beebarss.saj.ma@gmail.com>
2022-07-30 02:31:15 +0100

    Modified mocked request

--- server/todo-node/src/__tests__/taskController.test.ts
+++ server/todo-node/src/__tests__/taskController.test.ts
@@ -14,15 +14,7 @@ describe("Task controller", () => {
   it("handles errors correctly (POST)", async () => {
     const mockNext: NextFunction = jest.fn();
     const taskController = new TaskController();
-    const req = {
-      body: [
-        {
-          title: "Title",
-          description: "Description test",
-          complete: false,
-        },
-      ],
-    } as Request;
+    const req = {} as Request;
     const res = { send: jest.fn() } as unknown as Response;
 
     await taskController.saveTasks(req, res, mockNext);
